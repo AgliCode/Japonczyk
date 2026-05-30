bf2db642458b04c4257abb5d7340285b0329a02e
AgliCode <agatapolicht@gmail.com>
2026-05-30 15:29:51 +0200

    Add home entry for pawns

--- Scripts/pawn.gd
+++ Scripts/pawn.gd
@@ -7,10 +7,17 @@ extends Area2D
 @onready var game = $"../../GameManager"
 @onready var fields = get_node("/root/Game/Fields").get_children()
 @onready var bases = get_node("/root/Game/Bases").get_children()
+@onready var homes = get_node("/root/Game/Homes").get_children()
+
 
 @export var player_id = 0
 @export var base_index = 0
 @export var in_base = true
+
+var steps_taken = 0
+const HOME_SIZE = 4
+var in_home = false
+var home_index = -1
 
 func move_pawn(amount):
 	in_base = false
@@ -27,6 +34,7 @@ func leave_base():
 	in_base = false
 	current_field = start_field
 	position = fields[current_field].position
+	steps_taken = 0
 	
 
 func check_capture():
@@ -35,6 +43,8 @@ func check_capture():
 		if pawn == self:
 			continue
 		if pawn.in_base:
+			continue
+		if pawn.in_home:
 			continue
 		if pawn.player_id != player_id:
 			if pawn.current_field == current_field:
@@ -56,14 +66,31 @@ func _process(delta: float) -> void:
 	pass
 	
 func _input_event(viewport, event, shape_idx):
+	var my_home = homes[player_id].get_children()
 	if event is InputEventMouseButton:
 		if event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+			#Program sprawdza czy pionek jest w bazie i czy może z niej wyjść
 			if in_base and (game.dice_backward == 1 or game.dice_forward == 8):
 				leave_base()
 				return
-			if not in_base:
-				move_pawn(game.dice_forward)
-				check_capture()
+			if not in_base and not in_home:
+				var new_steps = steps_taken + game.dice_forward
+				#Program sprawdza czy pionek zrobił pełne okrążenie jeśli tak -> wchodzi do domku, jeśli nie-> robi normalny ruch
+				if new_steps >= fields.size():
+					var home_pos = new_steps - fields.size()
+					if home_pos >= HOME_SIZE:
+						return
+					else:
+						position = my_home[home_pos].position
+						in_home = true
+					
+				else:
+					move_pawn(game.dice_forward)
+					steps_taken = new_steps
+					check_capture()
 		if event.pressed and event.button_index == MOUSE_BUTTON_RIGHT:
-			move_pawn(-game.dice_backward)
-			check_capture()
+			if not in_base and not in_home:
+				var new_steps = steps_taken - game.dice_forward
+				steps_taken = new_steps
+				move_pawn(-game.dice_backward)
+				check_capture()
